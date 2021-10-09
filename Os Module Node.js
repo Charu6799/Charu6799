d967f3a53f918fd78a5e4a0a446b3b9be9924d81
Charu6799 <92023710+Charu6799@users.noreply.github.com>
2021-10-09 21:12:18 +0530

    Create Os Module Node.js

---
+++ Os Module Node.js
@@ -0,0 +1,9 @@
+ (7 sloc)  289 Bytes
+var os=require('os');
+console.log("1.CPU Architecture :"+ os.arch());
+console.log("2.Free Memory :"+ os.freemem());
+console.log("3.Total Memory :"+ os.totalmem());
+console.log("4.OS Platform :"+ os.platform());
+console.log("5.Current User Information :");
+console.log(os.userInfo());
+© 
