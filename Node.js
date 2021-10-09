23e36363884227cb319adf8ccf3291805cc69673
Charu6799 <92023710+Charu6799@users.noreply.github.com>
2021-10-09 19:09:22 +0530

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,11 @@
+const readline=require('readline');
+const rl=readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+const fs=require('fs');
+var filename="";
+var content="";
+var directoryName="";
+var removeDName="";
+var renamefile="";
